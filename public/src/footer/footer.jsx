c3578b3b6f84a1f18fe80bd984b4c953a7464d52
Vishal Saranathan <s-vishal1999@hotmail.com>
2020-08-30 21:45:31 +0530

    feat: Footer

---
+++ public/src/footer/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      Weaved with 🕸 by
+      <a className="spider-link" href="https://spider.nitt.edu">
+        {" "}
+        Spider
+      </a>
+    </footer>
+  );
+}
+export default Footer;

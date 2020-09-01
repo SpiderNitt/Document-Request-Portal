aac10c93fca20929d5f638d40af1eb0cac73262e
Vishal Saranathan <s-vishal1999@hotmail.com>
2020-09-01 18:54:57 +0530

    feat: Added nav component

--- public/src/App.js
+++ public/src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Login from "./login/login.jsx";
 import Footer from "./footer/footer.jsx";
 import Upload from "./student/cert-upload/cert-upl";
+import NavBar from "./student/navbar/navbar";
 
 function App() {
   return (
     <div>
+      <NavBar />
       {/* <Login /> */}
       <Upload />
       <Footer />

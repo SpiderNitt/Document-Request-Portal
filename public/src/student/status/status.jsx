e1adfb84fc8d8a3e18c6f9ed0b3098790bfb15c7
barath99 <exploretechsbk@gmail.com>
2020-09-03 00:31:56 +0530

    Removed node sass and converted scss to css

--- public/src/student/status/status.jsx
+++ public/src/student/status/status.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './status.scss'
+import './status.css'
 
 function Status() {
     return (
@@ -11,66 +11,78 @@ function Status() {
                 </div>
                 <div class="pre-class">
                 <ul class="timeline timeline-horizontal pre-class-1">
+                    
+                    
                     <li class="timeline-item">
-                        <div class="timeline-badge info"><i class="glyphicon glyphicon-check"></i></div>
+                        <div class="timeline-badge info"></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
                                 <h4 class="timeline-title">Mussum ipsum cacilds 3</h4>
-                                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago via Twitter</small></p>
+                                <p><small class="text-muted">11 hours ago via Twitter</small></p>
                             </div>
                             <div class="timeline-body">
                                 <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipisci. Mé faiz elementum girarzis, nisi eros gostis.</p>
                             </div>
                         </div>
                     </li>
+                    
+                    
                     <li class="timeline-item">
-                        <div class="timeline-badge danger"><i class="glyphicon glyphicon-check"></i></div>
+                        <div class="timeline-badge danger"></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
                                 <h4 class="timeline-title">Mussum ipsum cacilds 4</h4>
-                                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago via Twitter</small></p>
+                                <p><small class="text-muted"> 11 hours ago via Twitter</small></p>
                             </div>
                             <div class="timeline-body">
                                 <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis.</p>
                             </div>
                         </div>
                     </li>
+                    
+                    
                     <li class="timeline-item">
-                        <div class="timeline-badge danger"><i class="glyphicon glyphicon-check"></i></div>
+                        <div class="timeline-badge danger"></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
                                 <h4 class="timeline-title">Mussum ipsum cacilds 4</h4>
-                                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago via Twitter</small></p>
+                                <p><small class="text-muted"> 11 hours ago via Twitter</small></p>
                             </div>
                             <div class="timeline-body">
                                 <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis.</p>
                             </div>
                         </div>
                     </li>
+                   
+                   
                     <li class="timeline-item">
-                        <div class="timeline-badge warning"><i class="glyphicon glyphicon-check"></i></div>
+                        <div class="timeline-badge warning"></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
                                 <h4 class="timeline-title">Mussum ipsum cacilds 5</h4>
-                                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago via Twitter</small></p>
+                                <p><small class="text-muted"> 11 hours ago via Twitter</small></p>
                             </div>
                             <div class="timeline-body">
                                 <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis.</p>
                             </div>
                         </div>
                     </li>
+                    
+                    
                     <li class="timeline-item">
-                        <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+                        <div class="timeline-badge"></div>
                         <div class="timeline-panel">
                             <div class="timeline-heading">
                                 <h4 class="timeline-title">Mussum ipsum cacilds 6</h4>
-                                <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago via Twitter</small></p>
+                                <p><small class="text-muted"> 11 hours ago via Twitter</small></p>
                             </div>
                             <div class="timeline-body">
                                 <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis.</p>
                             </div>
                         </div>
                     </li>
+
+
                 </ul>
             </div>
             </div>

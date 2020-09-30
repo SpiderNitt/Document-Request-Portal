dddc2a008c06d3d9dbbb8f4dd62af1feebaa2860
N Praveen Chandhar <neevarp.star@gmail.com>
2020-09-30 16:53:51 +0530

    Add modifications for instructions modal

--- public/src/student/instructions-modal/instructions.js
+++ public/src/student/instructions-modal/instructions.js
@@ -1,48 +1,64 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
-
+import "./instructions.css"
 export default class InstructionsModal extends React.Component {
-  render() {
-    return (
-        <Modal
-            show={this.props.show}
-            onHide={this.props.hide}
-            keyboard={false}
-            dialogClassName="approveModal"
-            aria-labelledby="contained-modal-title-vcenter"
-            className="certModal"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Instructions
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <ol>
-                    <li>Select document type (Bonafide / Transcript / Course De-Registration / Course Re-Registration)<br /></li>
-                    <li>Enter Contact Number<br /></li>
-                    <li>Signatories' Email address pathway for the documents<br/>
-                        <ul>
-                            <li><strong>Bonafide</strong> : Warden -> HOD -> Associate Dean</li>
-                            <li><strong>Transcript</strong> : Default Path</li>
-                            <li><strong>Course De-Reg / Re-Reg</strong> : Faculty -> HOD -> Ugdean@nitt.edu -> Ugoffice@nitt.edu</li>
-                        </ul>
-                        <br />
-                        <strong>NOTE :</strong> Get the webmails of faculties from <a target="_blank" href="https://www.nitt.edu/">here</a><br />
-                        <br />
-                    </li>
-                    <li>Upload document
-                        Download the necessary documents :<br/>
-                        <ul>
-                            <li><strong>Bonafide</strong> : Formats for Download</li>
-                            <li><strong>Transcript</strong> : Fee Receipt</li>
-                            <li><strong>Course De-Reg / Re-Reg</strong> : Course Plan</li>
-                        </ul>
-                    </li>
-                </ol>
-            </Modal.Body>
-        </Modal>
-    );
-  }
+    render() {
+        return (
+            <Modal
+                show={this.props.show}
+                onHide={this.props.hide}
+                keyboard={false}
+                dialogClassName="approveModal"
+                aria-labelledby="contained-modal-title-vcenter"
+                className="certModal"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        <h3> Instructions</h3>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className='instructions-body'>
+                        <h5>GENERAL INSTRUCTIONS</h5>
+                        <ol>
+                            <li>
+                                <strong>Bonafide</strong>
+                                <ul>
+                                    <li><strong>Scan and upload</strong> the complete bonafide document.</li>
+                                    <li>Download the document from <a target="_blank" href='https://www.nitt.edu/home/academics/formats/BONAFIDE-CERTIFICATE-2016.pdf'><strong>here </strong></a></li>
+                                    <li><strong>Signatories</strong> : Warden → HOD → Associate Dean(adsw@nitt.edu)</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <strong>Transcript</strong>
+                                <ul>
+                                    <li>Upload the fee receipt after payment</li>
+                                    <li>Refer <a target="_blank" href="https://www.nitt.edu/home/academics/academic_documents/">here</a> for payment information</li>
+                                    <li><strong>Signatories</strong> : transcript@nitt.edu (No need to specify)</li>
+                                    <li>Choose a mode of delivery of document</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <strong>Course Re-Registraion/De-Registraion</strong>
+                                <ul>
+                                    <li>Upload the Course Registration form as obtained from MIS.</li>
+                                    <li><strong>Signatories</strong> : Faculty → HOD → ugacad@nitt.edu → ugsection@nitt.edu</li>
+                                </ul>
+                            </li>
+                        </ol>
+
+                        <h5>RELEVANT LINKS:</h5>
+                            <ul>
+                                <li>Get the webmails of faculties <a target="_blank" href="https://www.nitt.edu/home/academics/departments/faculty/">here</a></li>
+                                <li>Get the webmails of HODs <a target="_blank" href="https://www.nitt.edu/home/administration/hods/">here</a></li>
+                               
+                            </ul>
+
+
+                    </div>
+                </Modal.Body>
+            </Modal >
+        );
+    }
 }
--- public/src/student/instructions-modal/signatories-instructions.js
+++ public/src/student/instructions-modal/signatories-instructions.js
@@ -1,36 +1,41 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
-
+import "./instructions.css"
 export default class SignatoriesInstructionsModal extends React.Component {
-  render() {
-    return (
-        <Modal
-            show={this.props.show}
-            onHide={this.props.hide}
-            keyboard={false}
-            dialogClassName="approveModal"
-            aria-labelledby="contained-modal-title-vcenter"
-            className="certModal"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Instructions
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div>Signatories' Email address pathway for the documents</div>
-                <ol>
-                    <li><strong>Bonafide</strong> : Warden -> HOD -> Associate Dean</li>
-                    <li><strong>Transcript</strong> : Default Path</li>
-                    <li><strong>Course De-Reg / Re-Reg</strong> : Faculty -> HOD -> Ugdean@nitt.edu -> Ugoffice@nitt.edu</li>
-                </ol>
-                <br />
-                <div>
-                    <strong>NOTE :</strong> Get the webmails of faculties from <a target="_blank" href="https://www.nitt.edu/">here</a><br />
-                </div>
-            </Modal.Body>
-        </Modal>
-    );
-  }
+    render() {
+        return (
+            <Modal
+                show={this.props.show}
+                onHide={this.props.hide}
+                keyboard={false}
+                dialogClassName="approveModal"
+                aria-labelledby="modal-inst"
+                className="certModal"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        <h3>Order of Signatories</h3>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="instructions-body">
+                
+                        <ul>
+                            <li><strong>Bonafide</strong> : Warden → HOD → Associate Dean (adsw@nitt.edu)</li>
+                            <li><strong>Course De-Reg / Re-Reg</strong> : Faculty → HOD → ugacad@nitt.edu → ugsection@nitt.edu</li>
+                        </ul>
+                        <br />
+                        <div>
+                            <h4>RELEVANT LINKS:</h4>
+                            <ul>
+                                <li>Get the webmails of faculties <a target="_blank" href="https://www.nitt.edu/home/academics/departments/faculty/">here</a></li>
+                                <li>Get the webmails of HODs <a target="_blank" href="https://www.nitt.edu/home/administration/hods/">here</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        );
+    }
 }

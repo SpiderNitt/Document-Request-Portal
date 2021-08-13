67d3f74c52d0100fcdd4fc3d82fca0026e175d16
Smrithi23 <smrithi.prakash23@gmail.com>
2021-08-13 15:16:59 +0300

    Added status route

--- routers/api.js
+++ routers/api.js
@@ -8,4 +8,8 @@ api.use("/", middlewares.jwt_verify);
 api.use("/admin", adminRouter);
 api.use("/student", studentsRouter);
 
+api.get('/status', (req, res) => {
+	return res.status(200).json({ Success: true });
+});
+
 module.exports = api;
